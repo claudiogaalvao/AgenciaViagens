d8c433b6d09eaeb467bd2784382e24a3dcbf091d
cvascong <claudiogaalvao@gmail.com>
2020-06-10 13:02:02 -0300

    Ajuste de fluxo entre telas

--- app/src/main/java/br/com/claudiogalvao/agenciaviagens/ui/activity/PacotesViagemActivity.java
+++ app/src/main/java/br/com/claudiogalvao/agenciaviagens/ui/activity/PacotesViagemActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.List;
@@ -23,13 +24,19 @@ public class PacotesViagemActivity extends AppCompatActivity {
 
         configuraLista();
 
-        Intent intent = new Intent(this, ResumoPacoteActivity.class);
-        startActivity(intent);
+
     }
 
     private void configuraLista() {
         List<Pacote> pacotes = new PacoteDAO().lista();
         ListView listaPacotes = findViewById(R.id.activity_pacotes_viagem_listview);
         listaPacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
+        listaPacotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(PacotesViagemActivity.this, ResumoPacoteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/br/com/claudiogalvao/agenciaviagens/ui/activity/PagamentoActivity.java
+++ app/src/main/java/br/com/claudiogalvao/agenciaviagens/ui/activity/PagamentoActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
@@ -27,8 +29,14 @@ public class PagamentoActivity extends AppCompatActivity {
 
         mostraPreco(pacoteSaoPaulo);
 
-        Intent intent = new Intent(this, ResumoCompraActivity.class);
-        startActivity(intent);
+        Button botaoFinalizaCompra = findViewById(R.id.pagamento_botao_finaliza_compra);
+        botaoFinalizaCompra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PagamentoActivity.this, ResumoCompraActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void mostraPreco(Pacote pacote) {
